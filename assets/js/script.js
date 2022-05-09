0d0dbed9de8b4a0e2beb88b06bdcf8a1eebeb843
roo <roo116@nc.rr.com>
2022-05-09 04:45:23 -0400

    added code to make sure the button was targeted by the event of click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 //global variables
 //questions = []
+var timerEl = document.querySelector(".timer-section");
 
 
 
 //timer
-function timer(){cd
+function timer() {
     var sec = 30;
     var timer = setInterval(function(){
         document.getElementById('timer').innerHTML='00:'+sec;
@@ -15,7 +16,16 @@ function timer(){cd
     }, 1000);
 }
 
-document.getElementById("btn").addEventListener("click", timer());
+function btnHandler(click) {
+    //get target from event
+   var targetEl = event.target;
+
+    if(targetEl.matches("#btn")) {
+        timer();
+    }
+};
+
+timerEl.addEventListener("click", btnHandler);
 //high score
 
 //questions
