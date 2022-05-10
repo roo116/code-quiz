8d4be57dec238d7bd12a9073ddde76226d28cae8
roo <roo116@nc.rr.com>
2022-05-10 01:15:33 -0400

    testing out how to get questions to appear in browser window.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,13 @@
 //global variables
 //questions = []
 var timerEl = document.querySelector(".timer-section");
+var sec = 0;
+var hidden = document.getElementsByClassName("hidden");
+console.log(hidden);
+i = 1
 
 
 
-//timer
-function timer() {
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('timer').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
-}
 
 function btnHandler(click) {
     //get target from event
@@ -24,6 +17,30 @@ function btnHandler(click) {
         timer();
     }
 };
+
+//timer
+function timer() {
+    sec = 30;
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+
+    // askQuestions()
+}
+
+
+function askQuestions() {
+    i=1
+    document.getElementById("q[i]")
+
+
+    
+    
+}
 
 timerEl.addEventListener("click", btnHandler);
 //high score
