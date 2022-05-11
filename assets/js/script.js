a8f3add40cfb060f4f82ed5fad6dafcf8d2f2578
roo116 <roo116@nc.rr.com>
2022-05-11 16:09:31 -0400

    additional pseudocode for project in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,23 @@ document.getElementById("btn").addEventListener("click", timer());
 //high score
 
 //questions
+
+// put questions in an object. 1 object for a question.
+// properties a. answer, b. answer, c. answer, d. answer,
+// correct answer,
+//function to display right or wrong answer
+//
+
+// function to reduce timer on wrong answer
+// if answer = wrong reduce timer left by x;
+
+// function score
+// save score in local storage.  
+// maintain high score
+// show high score if necessary
+// log initials
+
+// create question cards from objects in HTML
+// create response that question is right or wrong
+// create code to hightlight selected response
+
