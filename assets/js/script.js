f50bd9dfbcadbc66666ffdbcf93e91ab89c8548e
roo116 <roo116@nc.rr.com>
2022-05-12 03:00:05 -0400

    added code to address acceptance criteria 1 clicking a button to start timer and ask question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,44 @@
 //global variables
 //questions = []
 
-// var timerEl = document.querySelector(".timer-section");
-// var sec = 0;
-// var hidden = document.getElementsByClassName("hidden");
+var timerEl = document.querySelector(".timer-section");
+var sec = 0;
+var hidden = document.getElementsByClassName("hidden");
 // console.log(hidden);
 // i = 1
 
-
 //timer
 
-// function btnHandler(click) {
-//     //get target from event
-//    var targetEl = event.target;
+function btnHandler(click) {
+    //get target from event
+   var targetEl = event.target;
 
-//     if(targetEl.matches("#btn")) {
-//         timer();
-//     }
-// };
+    if(targetEl.matches("#btn")) {
+        timer();
+    }
+};
 
-//timer
-// function timer() {
-//     sec = 30;
-//     var timer = setInterval(function(){
-//         document.getElementById('timer').innerHTML='00:'+sec;
-//         sec--;
-//         if (sec < 0) {
-//             clearInterval(timer);
-//         }
-//     }, 1000);
+timer
+function timer() {
+    sec = 60;
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 
-//     // askQuestions()
-// }
-
+    askQuestion()
+}
 
 // function askQuestions() {
 //     i=1
 //     document.getElementById("q[i]")
 
-
-
-
 // }
 
-// timerEl.addEventListener("click", btnHandler);
+
 //high score
 
 //questions
@@ -56,54 +51,98 @@
 
 //create questions objects as global and store in local storage
 var question1 = {
-    text: "What is your name?",
-    answerA: "Indiana Jones",
-    answerB: "Arthur, King of the Britons",
-    answerC: "A larch",
-    correctAnswer: "b"
-}
-
-var question2 = {
-    text: "What is your quest?",
-    answerA: "to find the Lost Ark of Israel",
-    answerB: "to learn fullstack development in 24 weeks",
-    answerC: "To seek the Holy Grail",
-    correctAnser: "c"
-}
-
-var questionList = [question1, question2]
-
-for (i = 0; i < questionList.length; i++) {
-    // console.log("q" + Number(i+1));
-    qdata = "q" + Number(i + 1);
-    localStorage.setItem(qdata, JSON.stringify(questionList[i]));
+  num: "1. ",
+  text: "What is your name?",
+  answerA: "Indiana Jones",
+  answerB: "Arthur, King of the Britons",
+  answerC: "A larch",
+  correctAnswer: "2",
 };
 
-//create first para element
-var createParaEl = document.createElement("p"); 
-document.querySelector(".qlist").appendChild(createParaEl);
-createParaEl.className = "q-text"
-// createLiEl.innerHTML = localStorage.getItem(JSON.parse(question1.text))  
-createParaEl.innerHTML = question1.text;
+var question2 = {
+  num: "2.",
+  text: "What is your quest?",
+  answerA: "to find the Lost Ark of the Covenent",
+  answerB: "to learn fullstack web-development in 24 weeks",
+  answerC: "To seek the Holy Grail",
+  correctAnswer: "c",
+};
 
-// create first answer
-document.querySelector(".qlist").appendChild(createParaEl);
-createParaEl.innerHTML = question1.answerA;
+//START - add question object to local storage
+
+// var questionList = [question1, question2];
+
+// for (i = 0; i < questionList.length; i++) {
+//     // console.log("q" + Number(i+1));
+//     qdata = "q" + Number(i + 1);
+//     localStorage.setItem(qdata, JSON.stringify(questionList[i]));
+// };
+
+//END - add question object to local storage
+
+//START - add question text to html
+
+function askQuestion() {
+var questionDivEl = document.querySelector(".question-div");
+var createQuestionEl = document.createElement("p");
+createQuestionEl.className = "question-item fs-3";
+createQuestionEl.textContent = question1.num + question1.text;
+questionDivEl.appendChild(createQuestionEl);
+// }
+
+//END - add question text to html
+
+// START - add answer text to html
+
+// function createAnswer() {
+var answerOlEl = document.querySelector("ol");
+var createAnswerElA = document.createElement("li");
+var createAnswerElB = document.createElement("li");
+var createAnswerElC = document.createElement("li");
+
+// for(i=0; i <= question1.length; i++) {
+// var createAnswerEl = document.createElement("li");
+
+//add answer a
+createAnswerElA.className = "fs-5";
+createAnswerElA.textContent = question1.answerA;
+answerOlEl.appendChild(createAnswerElA);
+// console.log(createAnswerEl);
+
+//add answer b
+createAnswerElB.className = "fs-5";
+createAnswerElB.textContent = question1.answerB;
+answerOlEl.appendChild(createAnswerElB);
+
+//add answer c
+createAnswerElC.className = "fs-5";
+createAnswerElC.textContent = question1.answerC;
+answerOlEl.appendChild(createAnswerElC);
+
+}
+
+//START answer question
 
 
+timerEl.addEventListener("click", btnHandler);
 
+// }
 
+//END add answer text to html
 
+// createLiEl.innerHTML = localStorage.getItem(JSON.parse(question1.text))
 
+//START pseudocode
 
-
-
+// // create first answer
+// document.querySelector(".qlist").appendChild(createParaEl);
+// createParaEl.innerHTML = question1.answerA;
 
 // function to reduce timer on wrong answer
 // if answer = wrong reduce timer left by x;
 
 // function score
-// save score in local storage.  
+// save score in local storage.
 // maintain high score
 // show high score if necessary
 // log initials
@@ -112,3 +151,4 @@ createParaEl.innerHTML = question1.answerA;
 // create response that question is right or wrong
 // create code to hightlight selected response
 
+//END pseudocode
